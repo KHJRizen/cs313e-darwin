ecc4a4691938dee8b6789838d62a45f4d3d21039
Kevin Jiang <kevin.jiang@utexas.edu>
2014-11-04 14:28:54 -0600

    close #13, fixed next_row and next_column check to properly increment

--- Darwin.py
+++ Darwin.py
@@ -24,6 +24,25 @@ class Darwin:
 			for c in range(self.columns):
 				if (self.grid[r][c] != 0 and not self.grid[r][c].checked):
 					print("hey")
+					
+					
+	def facing_wall(self, r, c):
+		nr = self.grid[r][c].next_row()
+		nc = self.grid[r][c].next_column()
+		return nr >= self.rows or nr < 0 or nc >= self.columns or nc < 0
+
+
+	def facing_empty(self, r, c):
+		nr = self.grid[r][c].next_row()
+		nc = self.grid[r][c].next_column()
+		return not self.facing_wall(r, c) and self.grid[nr][nc] == 0
+
+		
+	def facing_enemy(self):
+		self.direction = self.grid[r][c]
+		nr = next_row()
+		nc = next_column()
+		return not facing_wall and not facing_empty and self.grid[r][c].species != self.grid[nr][nc].species
 
 class Species:
 	def __init__(self, name, program=[]):
@@ -40,3 +59,21 @@ class Creature:
 		self.checked = False
 		self.r = r
 		self.c = c
+		
+	# gets row of one space in front of where creature is facing, given direction
+	def next_row(self):
+		if self.direction == 0 or self.direction == 2:
+			return self.r
+		if self.direction == 1:
+			return self.r + 1
+		else:
+			return self.r - 1
+
+	# gets column of one space in front of where creature is facing, given direction
+	def next_column(self):
+		if self.direction == 1 or self.direction == 3:
+			return self.c
+		if self.direction == 0:
+			return self.c + 1
+		else:
+			return self.c - 1
